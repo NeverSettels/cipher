175bcad5d530fce9e2e091ae510942553d26f1c5
Sean Downs <spdowns@protonmail.com>
2020-03-23 16:06:27 -0700

    add functinality to encode function to capitalize first and last leter

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   var sentence= prompt("enter a sentence to encode:");
 
   var encode = function(myString){
-    var tempString =  myString.charAt(0).toLocaleUpperCase()
-    tempString = tempString.charAt(tempString.length -1).toLocaleUpperCase()
-    return tempString
+    var myString =  myString.charAt(0).toUpperCase() + myString.substring(1)
+    var front = myString.slice(0, myString.length-1)
+    var tempString = front  + myString.charAt(myString.length -1).toUpperCase()
+    return tempString;
   }
 
 alert(encode(sentence));
