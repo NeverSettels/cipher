269ad805645faceda96cc59216b8b718aaf50470
Sean Downs <spdowns@protonmail.com>
2020-03-23 15:54:07 -0700

    return code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   var sentence= prompt("enter a sentence to encode:");
 
+  var encode = function(myString){
+    var tempString =  myString.charAt(0).toLocaleUpperCase()
+    tempString = tempString.charAt(tempString.length -1).toLocaleUpperCase()
+    return tempString
+  }
 
+alert(encode(sentence));
   
 });
